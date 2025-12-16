465a71601fc814b0d10129611561aeccbc07c86a
MustafaKemalCil <mst33kemal@gmail.com>
2025-12-16 11:23:12 +0300

    11:22 16.12.2025

--- mobil/lib/global.dart
+++ mobil/lib/global.dart
@@ -3,4 +3,4 @@ String apiBaseUrl = "http://10.33.121.124:8000";
 // "https://minimize-reduced-projection-drug.trycloudflare.com"; // Canlı sunucu
 //https://postings-ray-governing-possible.trycloudflare.com //cloudflare
 //String apiBaseUrl = "http://10.0.2.2:8000"; // Localhost Android emulator için
-String MOBILE_APP_VERSION = "2.5.0"; //uyuglamanın mevcut sürümü
+String MOBILE_APP_VERSION = "2.6.0"; //uyuglamanın mevcut sürümü
